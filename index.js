95115c97f0ec58c3cc70b48fd9518e449e2f6261
Lukasss895 <lukasluha3@gmail.com>
2026-08-28 22:58:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ const WELCOME_CHANNEL_ID = '1539635519359549574'; // Špeciálny kanál pre priv
 const WELCOME_CHANEL_ID2 = '1539662948157624320';
 
 const servers = [
-    { name: 'Server #1 (128-Tick)', host: '147.185.221.231', port: 42131 },
-    { name: 'Server #2 (64-Tick)', host: '147.185.221.231', port: 42183 },
+    { name: 'GO:COUNTER (128-Tick)', host: '147.185.221.231', port: 42131 },
+    { name: 'GO:COUNTER (64-Tick)', host: '147.185.221.231', port: 42183 },
     { name: 'GO:COUNTER Retakes (128-Tick)', host: '147.185.221.231', port: 42200 } // <-- Nová sekcia pre Retakes
 ];
 
